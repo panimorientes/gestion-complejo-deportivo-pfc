522955720e08c3504eaabc87ab9bb666f439e1c5
alecrugar@gmail.com <alecrugar@gmail.com@bc1500aa-7462-a4d7-6cc3-9edb9efde1fd>
2012-07-07 16:10:03 +0000

    [Ale] Todo subido

---
+++ trunk/src/domain/Persona.java
@@ -0,0 +1,51 @@
+package domain;
+
+public class Persona {
+	private int intId;
+	private String strNombres;
+	private String strApellidos;
+	private int intEdad;
+	private int intIdEmpresa;
+	private String strEstado;
+	
+	
+	public int getIntId() {
+		return intId;
+	}
+	public void setIntId(int intId) {
+		this.intId = intId;
+	}
+	public String getStrNombres() {
+		return strNombres;
+	}
+	public void setStrNombres(String strNombres) {
+		this.strNombres = strNombres;
+	}
+	public String getStrApellidos() {
+		return strApellidos;
+	}
+	public void setStrApellidos(String strApellidos) {
+		this.strApellidos = strApellidos;
+	}
+	public int getIntEdad() {
+		return intEdad;
+	}
+	public void setIntEdad(int intEdad) {
+		this.intEdad = intEdad;
+	}
+	public int getIntIdEmpresa() {
+		return intIdEmpresa;
+	}
+	public void setIntIdEmpresa(int intIdEmpresa) {
+		this.intIdEmpresa = intIdEmpresa;
+	}
+	public String getStrEstado() {
+		return strEstado;
+	}
+	public void setStrEstado(String strEstado) {
+		this.strEstado = strEstado;
+	}
+
+		
+
+}
