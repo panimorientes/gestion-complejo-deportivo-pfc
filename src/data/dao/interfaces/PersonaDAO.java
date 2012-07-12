732901f88378183824ad96970796943a120185c4
alecrugar <alecrugar@gmail.com>
2012-07-12 17:41:13 +0000

    [Ale] Funcionando TODO Integrado :)

--- src/data/dao/interfaces/PersonaDAO.java
+++ src/data/dao/interfaces/PersonaDAO.java
@@ -5,5 +5,9 @@ import domain.interfaces.Persona;
 public interface PersonaDAO {
 
 	public abstract void insertar(Persona p);
+	
+	public abstract void actualizar(Persona p);
+	
+	public abstract Persona obtener(int id);
 
 }
